ff262b5d8f1b2857cbb02e90394d6eb53d71bb6e
Madyan <madyaneka@protonmail.com>
2021-05-05 04:19:10 +0700

    add mysql driver; testing db connection

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { Sequelize } = require('sequelize')
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,3 +7,16 @@ const port = 3000
 app.listen(port, () => {
   console.log('Running on port ' + port)
 })
+
+
+// Testing Database connection
+const db = require('./src/config/database.json')
+const sequelize = new Sequelize(db.development)
+
+
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
