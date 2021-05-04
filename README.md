24f6adc800585bb8286ff78124cecdf71d8522b2
Madyan <madyaneka@protonmail.com>
2021-05-05 05:54:11 +0700

    create user model

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # How to Run
 
 1. Clone this project and install its dependencies `npm install`
-2. Start the app with `npm start`
+2. Migrate models `npx sequelize-cli db:migrate`
+3. Start the app with `npm start`
---
+++ src/db/migrations/20210504215859-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      fullname: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'creator', 'participant')
+      },
+      deleted_at: {
+        type: Sequelize.DATE
+      },
+      confirmed_at: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
