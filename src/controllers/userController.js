23e5070a67f0ccb58044da857230c092529452dc
Madyan <madyaneka@protonmail.com>
2021-05-05 22:47:12 +0700

    get all users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,17 @@ const { User } = require('../db/models')
 
 class userController {
 
+  static async getUsers(req, res, next) {
+    const users = await User.findAll()
+    if (users) {
+      res.json({
+        sucess: true,
+        message: 'success retrieving data',
+        data: users
+      })
+    }
+  }
+
   static async getUserById(req, res, next) {
     try {
       const data = await User.findByPk(req.params['id'])
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,8 @@ const router = express.Router()
 // API Endpoints
 router.post('/api/auth/register', registerController.register)
 router.post('/api/auth/login', registerController.login)
+
+router.get('/api/users', [jwt, authorization('admin')], userController.getUsers)
 router.get('/api/users/:id', [jwt, authorization('admin')], userController.getUserById, afterMiddleware)
 
 
