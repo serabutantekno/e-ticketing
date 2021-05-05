ce73c43b57591ef3208d7eb866bc6e0907c867eb
Madyan <madyaneka@protonmail.com>
2021-05-05 07:27:46 +0700

    jwt auth - an example

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 const { Sequelize } = require('sequelize')
+const dotenv = require('dotenv').config()  // get config variables
+const jwt = require('jsonwebtoken')
 const express = require('express')
 const app = express()
 const port = 3000
+
+const middlewares = require('./src/middlewares/middlewares')
+
+
+app.use(express.json())
+
+
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '60s' });
+}
+
+
+app.post('/api/createNewUser', (req, res) => {
+  // ...
+
+  console.log(req.body)
+
+  const token = generateAccessToken({ username: req.body.username });
+  res.json(token)
+
+  // ...
+});
+
+
+app.get('/api/whatever', middlewares, (req, res) => {
+  const token = req.header('eticketing-jwt')
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      res.status(500).send({
+        auth: false,
+        message: 'Gagal autentikasi token.'
+      })
+    } else {
+      res.status(200).send(decoded)
+    }
+  })
+
+  res.sendStatus(200)
+})
 
 
 app.listen(port, () => {
---
+++ src/middlewares/jwt.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers.authorization
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403)
+      }
+
+      req.user = user
+      next()
+    })
+  } else {
+    res.sendStatus(401)
+  }
+}
+
+module.exports = authenticateToken
