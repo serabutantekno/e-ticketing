c7c1c38ebe275c5538e02388303e6c45d1574159
Madyan <madyaneka@protonmail.com>
2021-05-26 09:13:09 +0700

    job scheduler send payment instruction mail every 12 AM

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const helper = require('./src/helpers')
 require('dotenv').config({ path: helper.fileExists('.env_secret') })  // get environment variables
 const express = require('express')
 const app = express()
+const db = require('./src/db/models')
 const cors = require('cors')
+const cron = require('node-cron')
+const Queue = require('bull')
+const sendMail = require('./src/controllers/sendMailController')
+const sendInstructionPaymentMail = new Queue('sendInstructionPaymentMail')
+sendInstructionPaymentMail.process(2, (job, done) => {
+  sendMail(job.data.email, 'emailPaymentInstruction24H').then(res => done(null, res))
+})
 const port = 3000
 
 const routes = require('./src/routes/routes')
+
 
 var corsOptions = {
   origin: ['https://developer.mozilla.org', 'https://www.google.com'],
@@ -24,6 +33,18 @@ app.use((err, req, res, next) => {
     message: err.message,
     stack_error: err
   })
+})
+
+
+cron.schedule('0 12 * * *', async () => {
+  /** Promotional Mails */
+  const query = `SELECT e.id, u.username, u.email, p.payment_status, e.event_start_date, e.event_end_date
+  FROM Payments p
+  JOIN Users u ON p.participant_id = u.id
+  JOIN Events e ON p.event_id = e.id
+  WHERE DATE_SUB(CURDATE(), INTERVAL 1 DAY) <= e.event_start_date AND p.payment_status != 'passed'`
+  const [result, metadata] = await db.sequelize.query(query)
+  sendInstructionPaymentMail.add({email: result[0].email})
 })
 
 
